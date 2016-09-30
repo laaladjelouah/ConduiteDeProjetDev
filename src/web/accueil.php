2ceb803628459dfde4cd2d44733bd4c45f71bd5f
slaveau <slaveau@u-bordeaux.fr>
2016-09-30 11:29:19 +0200

    Création de assets + quelques fichiers necessaires pour T3

---
+++ src/web/accueil.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>	
+	<meta charset="UTF-8">
+		
+	<title> Accueil </title>
+	
+	<link href="assets/css/accueil.css" rel="stylesheet" media="screen">
+    </head>
+
+    <body>
+	
+	<h1>Accueil</h1>
+
+
+
+    </body>
+
+</html>
